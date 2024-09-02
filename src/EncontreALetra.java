2b6642b81deedd0a52db833c30ad069b4497c738
Igo Marcelino <73483348+igomarcelino@users.noreply.github.com>
2024-09-02 10:21:45 -0300

    Adicionados comentarios sobre funcionalidades no codigo

--- src/EncontreALetra.java
+++ src/EncontreALetra.java
@@ -8,6 +8,7 @@ public class EncontreALetra {
         System.out.print("\nAgora informe uma letra para verificar se a palavra "+palavra+" contem e quantas vezes aparece: ");
         String letra =teclado.nextLine().toLowerCase();// deixa todas letras minusculas
         int encontradaXVezes = acharEContarLetra(palavra,letra);
+        // esse if mostra se foi encontrado ou nao , caso encontrado mostra a quantidade de vez que a letra apareceu.
         if (encontradaXVezes!=0){
             System.out.println("A letra "+ letra+" foi encontrada na palavra( "+palavra+" ) "+encontradaXVezes+" vez(es)");
         }else {
@@ -15,6 +16,10 @@ public class EncontreALetra {
         }
     }
 
+    /**
+        Esse metodo recebe uma palavra e uma letra , o mesmo verifica a cada indice da palavra para ver se 
+        o char que contem naquele indice e igual ao char informado na String letra para ser procurado
+    */
     public static int acharEContarLetra(String palavra,String letra){
         int contagem = 0;
         for (int i = 0; i < palavra.length();i++){
